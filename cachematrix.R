845bc3ad2b14a140b5e959e5c95088c62757c137
Nathan Charles <ncharles@gmail.com>
2015-07-24 15:35:14 -0400

    updated default functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## speed up matrix inverse by cacheinq
 
-## Write a short comment describing this function
+## example:
+
+## c <- makeCacheMatrix(matrix(1:4,2,2))
+
+## cacheSolve(c)
+
+## cacheSolve(c)
+
+
+## return a matrix that is cacheable
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
+## solve for matrix inverse after checking for a cached solution
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
